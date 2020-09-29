792c36da632f32b882dc1fdd9a0917e2aae7bd79
EasyArch-lyy <1480921056@qq.com>
2020-09-29 19:25:29 +0800

    修改

---
+++ malltest-redis/src/main/java/com/mall/malltestredis/MalltestRedisApplication.java
@@ -0,0 +1,13 @@
+package com.mall.malltestredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalltestRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalltestRedisApplication.class, args);
+    }
+
+}
---
+++ malltest-redis/src/test/java/com/mall/malltestredis/MalltestRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.malltestredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalltestRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
